4369f46664c4804dcfe73b69ac8c06891a92a4fc
WolfByteNS <wolfbytens@gmail.com>
2020-11-25 19:37:06 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace blockedNOT
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.RPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(132, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "blockedNOT";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(181, 69);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(175, 20);
+            this.username.TabIndex = 1;
+            this.username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(181, 112);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(175, 20);
+            this.password.TabIndex = 2;
+            this.password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(181, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // RPassword
+            // 
+            this.RPassword.Location = new System.Drawing.Point(181, 138);
+            this.RPassword.Name = "RPassword";
+            this.RPassword.PasswordChar = '*';
+            this.RPassword.Size = new System.Drawing.Size(175, 20);
+            this.RPassword.TabIndex = 4;
+            this.RPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label2.Location = new System.Drawing.Point(88, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label3.Location = new System.Drawing.Point(93, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label4.Location = new System.Drawing.Point(38, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Retype Password:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(94, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(269, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "-If button doesn\' t work, then the passwords dont match";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Extras";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Blue;
+            this.label6.Location = new System.Drawing.Point(177, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 37);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "extras";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Ultrasurf";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(398, 225);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Back";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(91, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(300, 108);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Done!";
+            // 
+            // label8
+            // 
+            this.label8.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(0, 112);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(476, 140);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Your username will be ";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(87, 74);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(88, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Browser Unblock";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(195, 112);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Uninstaller";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(71, 117);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(104, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Forticlient Uninstaller";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label11.Location = new System.Drawing.Point(155, 46);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(147, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Administrator Account Creator";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(195, 217);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "<- Back";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(1, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(101, 13);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "Copyright Demonfall";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(476, 252);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RPassword);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "blockedNOT";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox RPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace blockedNOT
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string usernameText = username.Text;
+            string passwordText = password.Text;
+            string repass = RPassword.Text;
+
+            if (usernameText != "")
+            {
+                if (passwordText == repass)
+                {
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"accountcreation.bat"))
+                    {
+                        file.WriteLine("net users " + usernameText + " " + passwordText + " /add");
+                        file.WriteLine("net localgroup administrators " + usernameText + " /add");
+                    }
+                    username.Text = "";
+                    password.Text = "";
+                    ExecuteAsAdmin("accountcreation.bat");
+                    label1.Hide();
+                    label2.Hide();
+                    label3.Hide();
+                    label4.Hide();
+                    label5.Hide();
+                    label11.Hide();
+                    username.Hide();
+                    password.Hide();
+                    RPassword.Hide();
+                    button1.Hide();
+                    button2.Hide();
+                    label7.Show();
+                    label8.Text = label8.Text + Environment.MachineName + "/" + usernameText + "\nYour password is the one you set";
+                    label8.Show();
+                    button6.Show();
+                    Thread.Sleep(500);
+                    File.Delete(@"accountcreation.bat");
+                }
+            }
+        }
+
+        public void ExecuteAsAdmin(string fileName)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = fileName;
+            proc.StartInfo.UseShellExecute = true;
+            proc.StartInfo.Verb = "runas";
+            proc.Start();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            label1.Hide();
+            label2.Hide();
+            label3.Hide();
+            label4.Hide();
+            label5.Hide();
+            username.Hide();
+            password.Hide();
+            RPassword.Hide();
+            button1.Hide();
+            button2.Hide();
+            label6.Show();
+            button3.Show();
+            button4.Show();
+            label9.Show();
+            label10.Show();
+            button5.Show();
+            label11.Hide();
+            username.Text = "";
+            password.Text = "";
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            label6.Hide();
+            button3.Hide();
+            button4.Hide();
+            label7.Hide();
+            label8.Hide();
+            label9.Hide();
+            button5.Hide();
+            label10.Hide();
+            button6.Hide();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            label1.Show();
+            label2.Show();
+            label3.Show();
+            label4.Show();
+            label5.Show();
+            username.Show();
+            password.Show();
+            RPassword.Show();
+            button1.Show();
+            button2.Show();
+            label6.Hide();
+            button3.Hide();
+            button4.Hide();
+            label9.Hide();
+            label10.Hide();
+            button5.Hide();
+            label11.Show();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.techspot.com/downloads/downloadnow/5711/?evp=9ac22ecd801a0ccf9ee380fd5ca119a7&file=1");
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://download1592.mediafire.com/wzgqx48chsng/6npiopibghmz918/uninstaller.exe");
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            label1.Show();
+            label2.Show();
+            label3.Show();
+            label4.Show();
+            label5.Show();
+            label11.Show();
+            username.Show();
+            password.Show();
+            RPassword.Show();
+            button1.Show();
+            button2.Show();
+            label7.Hide();
+            label8.Hide();
+            button6.Hide();
+        }
+    }
+}
